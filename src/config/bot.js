fe2301a6a6f4b0faa093fab11a6f68135c304cd1
flighteam <crosbydonghia1@gmail.com>
2026-06-13 14:14:38 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "made by skibidi rip",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 3, 
       },
     ],
   },
@@ -88,8 +88,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
-      secondary: "#2F3136", 
+      primary: "#0F0616", 
+      secondary: "#00000", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
